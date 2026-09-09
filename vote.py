6e23e2e2253ff2a666df308aac955466872df8cd
pragatidivanadavar12-boop <pragatidivanadavar12@gmail.com>
2026-09-09 11:38:09 +0530

    Update vote.py

--- vote.py
+++ vote.py
@@ -1,5 +1,7 @@
 age=int(input("enter your age"))
 if(age>=18):
   print("eligible for voting")
+  print("you can register as voter")
 else:
   print("not eligible for voting")
+  print("you must be at least 18 years old")
